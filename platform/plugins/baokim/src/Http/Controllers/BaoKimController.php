d6582ca1e5eaf5931f20ec41e97ae2b2988da65b
Cao Anh Nhất <an.dev2504@gmail.com>
2021-04-10 01:40:15 +0700

    fix bao kim

--- platform/plugins/baokim/src/Http/Controllers/BaoKimController.php
+++ platform/plugins/baokim/src/Http/Controllers/BaoKimController.php
@@ -30,19 +30,20 @@ class BaoKimController extends BaseController
         ]);
 
         if ($verifyQuery) {
-            $orderDetail    = app(BaoKimAPI::class)->getOrderDetail($request->input('id'), $request->input('mrc_order_id'));
-            $data = $orderDetail->data;
-            if ($orderDetail->code != 0) return $response
-                ->setError()
-                ->setNextUrl(env('FE_SITE_URL', url('/')) . '/thanh-toan-that-bai.html?' . $responseQuery);
-
             $prefix = get_ecommerce_setting('store_order_prefix') ? get_ecommerce_setting('store_order_prefix') . '-' : '';
             $suffix = get_ecommerce_setting('store_order_suffix') ? '-' . get_ecommerce_setting('store_order_suffix') : '';
             $orderId    =   (int)Str::replaceFirst($suffix, "", Str::replaceFirst($prefix, "", Str::replaceFirst("#", "", $request->input('mrc_order_id')))) - (int)config('plugins.ecommerce.order.default_order_start_number');
-
+            $orderDetail = app(BaoKimAPI::class)->getOrderDetail($request->input('id'), $request->input('mrc_order_id'));
             $status = PaymentStatusEnum::PENDING;
 
-            switch ($data->stat) {
+            $data = $orderDetail->data;
+            // if ($orderDetail->error ||  $orderDetail->code != 0) return $response
+            //     ->setError()
+            //     ->setNextUrl(env('FE_SITE_URL', url('/')) . '/dat-hang-that-bai.html?' . $responseQuery);
+
+          
+
+            switch ($request->input("stat")) {
                 case "p":
                 case "r":
                     $status = PaymentStatusEnum::PENDING;
@@ -56,19 +57,18 @@ class BaoKimController extends BaseController
             }
 
             $this->storeLocalPayment([
-                'amount'          => $data->total_amount,
+                'amount'          => $data->total_amount ?? $request->input("total_amount"),
                 'currency'        => "VND",
-                'charge_id'       => $data->id,
+                'charge_id'       => $data->id ?? $request->input("id"),
                 'payment_channel' => BAOKIM_PAYMENT_METHOD_NAME,
                 'status'          => $status,
-                'customer_id'     => auth('customer')->check() ? auth('customer')->user()->getAuthIdentifier() : null,
-                'payment_type'    => $data->bpm_id,
+                'payment_type'    => $data->bpm_id ?? null,
                 'order_id'        => $orderId,
             ]);
 
-            OrderHelper::processOrder($orderId, $data->id);
+            OrderHelper::processOrder($orderId, $request->input('id'));
 
-            if ($data->stat != "c") {
+            if ($request->input("stat") != "c") {
                 return $response
                     ->setNextUrl(env('FE_SITE_URL', url('/')) . '/dat-hang-that-bai.html?' . $responseQuery);
             }
